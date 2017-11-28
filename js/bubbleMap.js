7be3b05b35006d6786592d59e1b197baf4aa29ab
kmayur3 <31552153+kmayur3@users.noreply.github.com>
2017-11-28 18:44:12 -0500

    UpdateBubbleMap

--- js/bubbleMap.js
+++ js/bubbleMap.js
@@ -1,22 +1,47 @@
 var bubbleChart;
 var height = 1200;
 var width = 1200;
+var flag = 0
 
-
-function show()
+function show(flag)
 {
+	
 	console.log("Inside show")
-	bubbleChart = d3.select('#divContainer').select('#bubbleMap').append('svg').attr("height", height).attr("width", width);
-	bubbleMap = bubbleChart.append('g');
-	d3.csv('data/bubbleMapCountryData.csv', showBubbleMap);
+	
+	
 
+	if(flag==1)
+	{
+		let div = document.getElementById('bubbleMap');
+		let childNodes = div.childNodes; 
+
+		//clear existing SVG element if any 
+		for(var i=0; i<childNodes.length; i++)
+		{
+			let el = childNodes[i];
+			if(el instanceof SVGElement){
+				$(el).remove();
+			}
+		}
+
+
+	}
+
+    bubbleChart = d3.select('#divContainer').select('#bubbleMap').append('svg').attr("height", height).attr("width", width);
+	bubbleMap = bubbleChart.append('g');
+
+	d3.csv('data/bubbleMapCountryData.csv', showBubbleMap);
 }
 
 
 
 function showBubbleMap(rawdata)
 {
-	console.log("Hello")
+
+
+
+	var selectedFeature = document.getElementById('dropDownAudioFeatures').value
+	console.log(selectedFeature)
    
     var initialScaleDataX =[]
     var initialScaleDataY = []
@@ -47,7 +72,8 @@ function showBubbleMap(rawdata)
                             .domain([minDataPointY,maxDataPointY])
                             .range([0+ypadding,500-ypadding]);
     
-  
+    
+   
 
    
 	bubbleChart.selectAll("circle")
@@ -56,10 +82,24 @@ function showBubbleMap(rawdata)
 	.append("circle")
 	.style("stroke", "gray")
     .style("fill", "green")
-    .attr("r",function(d){return d.Radius})
+    .attr("r",function(d){ 
+    	if (selectedFeature == "Valence")
+    	{	
+    		console.log(d.Valence)
+    		return d.Valence;
+    	}
+    	if (selectedFeature == "Energy")
+    	{
+    		console.log(d.Energy)
+			return d.Energy;
+		}
+		else 
+			return 0})
 	.attr("cy",function(d){return 500-linearScaleX((d.X) *1000)})
 	.attr("cx",function(d){return  linearScaleY((d.Y)*1000)})
 	.attr("text",function(d){return d.Country})
+
+
 	 
 
 
